2ae744d731b0adb8c05b4190e1d636ffb465efd0
John Ouellet <john@thinktandem.io>
2022-02-17 11:49:41 -0500

    Start of docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,29 +1,62 @@
 const {path} = require('@vuepress/utils');
-const yaml = require('js-yaml');
-const fs = require('fs');
 
 module.exports = {
   lang: 'en-US',
   title: 'Lando',
-  description: 'Lando Pantheon Plugin Documentation',
+  description: 'Lando is the best local development environment option for Pantheon, the fastest way to build modern web apps.',
+  base: '/pantheon/',
   head: [
     ['meta', {name: 'viewport', content: 'width=device-width, initial-scale=1'}],
-    ['link', {rel: 'preconnect', href: '//fonts.googleapis.com/css?family=Poppins:700|Source+Sans+Pro&display=swap', crossorigin: 'true'}],
-    ['link', {rel: 'icon', href: '/favicon.ico'}],
-    ['link', {rel: 'stylesheet', href: '//fonts.googleapis.com/css?family=Poppins:700|Source+Sans+Pro&display=swap'}],
+    ['link', {rel: 'icon', href: '/pantheon/favicon.ico', size: 'any'}],
+    ['link', {rel: 'icon', href: '/pantheon/favicon.svg', type: 'image/svg+xml'}],
+    ['link', {rel: 'preconnect', href: '//fonts.googleapis.com'}],
+    ['link', {rel: 'preconnect', href: '//fonts.gstatic.com', crossorigin: true}],
+    ['link', {rel: 'stylesheet', href: '//fonts.googleapis.com/css2?family=Lexend:wght@500&display=swap'}],
   ],
-  theme: '@lando/vuepress-theme-lando-docs',
+  theme: '@lando/vuepress-theme-default-plus',
   themeConfig: {
-    logo: '/images/logo-pink-small.png',
-    repo: 'lando/pantheon',
+    landoDocs: true,
+    logo: '/images/icon.svg',
     docsDir: 'docs',
     docsBranch: 'main',
-    sponsors: yaml.load(fs.readFileSync(path.resolve(__dirname, 'public') + '/api/sponsors.yml', 'utf8')),
-    // showSponsors: ['platformsh'],
-    showCarbonAds: true,
-    showSponsors: true,
-    navbar: [
-      {text: 'Getting Started', link: 'https://docs.lando.dev/basics/'},
+    repo: 'lando/pantheon',
+    sidebarHeader: {
+      enabled: true,
+      title: 'Pantheon Plugin',
+      icon: '/images/pantheonicon.png',
+    },
+    sidebar: [
+      {
+        text: 'Overview',
+        link: '/index.md',
+      },
+      '/getting-started.md',
+      '/config.md',
+      '/tooling.md',
+      '/sync.md',
+      '/caveats.md',
+      {
+        text: 'Guides',
+        collapsible: true,
+        children: [
+          {
+            text: 'Adding more tooling commands',
+            link: '/adding-more-tooling.md',
+          },
+          {
+            text: 'Externally accessing services',
+            link: '/external-access.md',
+          },
+          {
+            text: 'Manually importing databases',
+            link: '/manually-importing-databases.md',
+          },
+        ],
+      },
+      '/support.md',
+      {text: 'Examples', link: 'https://github.com/lando/pantheon/tree/main/examples'},
+      {text: 'Release Notes', link: 'https://github.com/lando/pantheon/releases'},
+      '/development.md',
     ],
   },
 };
