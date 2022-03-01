63bed4120d178842cde4a9c1f64514e488dc6549
Alec Reynolds <alec+git@lando.dev>
2022-03-01 11:11:11 -0800

    #17: Use redis:6 on M1 Macs.

--- recipes/pantheon/builder.js
+++ recipes/pantheon/builder.js
@@ -131,7 +131,7 @@ module.exports = {
       options.defaultFiles.vhosts = `${options.framework}.conf.tpl`;
       options = overrideAppserver(options);
       // Add in cache if applicable
-      if (options.cache) options = _.merge({}, options, utils.getPantheonCache());
+      if (options.cache) options = _.merge({}, options, utils.getPantheonCache(isArmed));
       // Add in edge if applicable
       if (options.edge) options = _.merge({}, options, utils.getPantheonEdge(options));
       // Add in index if applicable
