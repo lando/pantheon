6f44ab9592efda6f31a0f169ea7fab651a351dae
Alec Reynolds <alec+git@lando.dev>
2023-11-30 21:59:28 -0800

    Remove extra settings for the regular non-arm mariadb.

--- builders/pantheon-mariadb-arm.js
+++ builders/pantheon-mariadb-arm.js
@@ -2,20 +2,37 @@
 
 // Modules
 const _ = require('lodash');
-const pinPairs = {
-  pinPairs: {
-    '10.6': 'mariadb:10.6.7',
-    '10.4': 'mariadb:10.4.21',
-    '10.3': 'mariadb:10.3.31',
-    '10.1': 'mariadb:10.1.48',
-  },
-};
-const dbConfig = _.merge({}, require('../lib/utils').getPantheonDbConfig, pinPairs);
+const path = require('path');
 
 // Builder
 module.exports = {
   name: 'pantheon-mariadb-arm',
-  config: dbConfig,
+  config: {
+    version: '10.3', // @todo: this will probably be 10.3, check D6 support.
+    supported: ['10.6', '10.4', '10.3', '10.1'],
+    legacy: ['10.1'],
+    pinPairs: {
+      '10.6': 'mariadb:10.6.7',
+      '10.4': 'mariadb:10.4.21',
+      '10.3': 'mariadb:10.3.31',
+      '10.1': 'mariadb:10.1.48',
+    },
+    patchesSupported: true,
+    confSrc: path.resolve(__dirname, '..', 'config'),
+    creds: {
+      database: 'pantheon',
+      password: 'pantheon',
+      user: 'pantheon',
+    },
+    healthcheck: 'mysql -uroot --silent --execute "SHOW DATABASES;"',
+    port: '3306',
+    defaultFiles: {
+      database: 'my_custom.cnf',
+    },
+    remoteFiles: {
+      database: '/etc/mysql/cnf.d/my_custom.cnf',
+    },
+  },
   parent: '_service',
   builder: (parent, config) => class PantheonMariaDbArm extends parent {
     constructor(id, options = {}) {
--- builders/pantheon-mariadb.js
+++ builders/pantheon-mariadb.js
@@ -2,13 +2,11 @@
 
 const _ = require('lodash');
 const LandoMariadb = require('@lando/mariadb/builders/mariadb.js');
-const dbConfig = require('./../lib/utils').getPantheonDbConfig;
 
 // Builder
 module.exports = {
   name: 'pantheon-mariadb',
   parent: '_service',
-  config: dbConfig,
   builder: (parent, config) => class PantheonMariadb extends LandoMariadb.builder(parent, LandoMariadb.config) {
     constructor(id, options = {}) {
       options = _.merge({}, config, options);
