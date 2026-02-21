9e6fa64eae569ee78c6f412c5014105f5920d2fb
Ethan Georgi <65237656+ethangeorgi@users.noreply.github.com>
2026-02-21 18:14:59 -0500

    #215 lando pull -vvv (#234)
    * #215 lando pull -vvv
    
    * adding --debug for maximum verbosity
    
    * fix: add verbosity flag to all remaining terminus commands
    
    Adds $VERBOSITY to terminus commands that were missed:
    
    pull.sh:
    - connection:info (db fallback pull command)
    - backup:get (file download)
    
    push.sh:
    - env:info (connection mode check)
    - env:diffstat (uncommitted changes check)
    - auth:whoami (git config setup)
    - connection:info (db push connection)
    
    * docs: add changelog entry for verbosity flag passthrough
    
    ---------
    
    Co-authored-by: Aaron Feledy <aaron@arrow.one>

--- scripts/pull.sh
+++ scripts/pull.sh
@@ -23,6 +23,7 @@ DATABASE=${TERMINUS_ENV:-dev}
 FILES=${TERMINUS_ENV:-dev}
 RSYNC=false
 NO_AUTH=false
+VERBOSITY=""
 
 # Set helpers
 FRAMEWORK=${FRAMEWORK:-drupal}
@@ -77,6 +78,14 @@ while (( "$#" )); do
         NO_AUTH=true
         shift
       ;;
+    -vv|--verbose)
+        VERBOSITY="-vv"
+        shift
+      ;;
+    -vvv|--debug)
+        VERBOSITY="-vvv"  
+        shift
+      ;;
     --)
       shift
       break
@@ -100,7 +109,7 @@ fi
 if [ "$CODE" != "none" ]; then
   # Validate before we begin
   lando_pink "Validating you can pull code from $CODE..."
-  terminus env:info $SITE.$CODE
+  terminus env:info $VERBOSITY $SITE.$CODE
   lando_green "Confirmed!"
 
   # Get the git branch
@@ -126,7 +135,7 @@ fi
 # Get the database
 if [ "$DATABASE" != "none" ]; then
   # Holla at @uberhacker for this fu
-  FALLBACK_PULL_DB="$(echo $(terminus connection:info $SITE.$DATABASE --field=mysql_command) | sed 's,^mysql,mysqldump --no-autocommit --single-transaction --opt -Q,')"
+  FALLBACK_PULL_DB="$(echo $(terminus connection:info $VERBOSITY $SITE.$DATABASE --field=mysql_command) | sed 's,^mysql,mysqldump --no-autocommit --single-transaction --opt -Q,')"
   LOCAL_MYSQL_CONNECT_STRING="mysql --user=pantheon --password=pantheon --database=pantheon --host=database --port=3306"
 
   # Make sure the terminus command returned from buildDbPullCommand() has the
@@ -135,7 +144,7 @@ if [ "$DATABASE" != "none" ]; then
 
   # Validate before we begin
   lando_pink "Validating you can pull the database from $DATABASE..."
-  terminus env:info $SITE.$DATABASE
+  terminus env:info $VERBOSITY $SITE.$DATABASE
   lando_green "Confirmed!"
 
   # Destroy existing tables
@@ -153,7 +162,7 @@ EOF
 
   # Wake up the database so we can actually connect
   lando_pink "Making sure your site is awake..."
-  terminus env:wake $SITE.$DATABASE
+  terminus env:wake $VERBOSITY $SITE.$DATABASE
 
   # Importing database
   echo "Pulling your database... This miiiiight take a minute"
@@ -185,7 +194,7 @@ if [ "$FILES" != "none" ]; then
 
   # Validate before we begin
   lando_pink "Validating you can pull files from $FILES..."
-  terminus env:info $SITE.$FILES
+  terminus env:info $VERBOSITY $SITE.$FILES
   lando_green "Confirmed!"
 
   # Make sure the filemount actually exists
@@ -212,13 +221,13 @@ if [ "$FILES" != "none" ]; then
     $LANDO_WEBROOT/$FILEMOUNT"
 
   # Verify we have a files dump, it not let's switch to rsync mode
-  if ! terminus backup:list $SITE.$FILES --element=files --format=list | grep "files" 2>&1; then
+  if ! terminus backup:list $VERBOSITY $SITE.$FILES --element=files --format=list | grep "files" 2>&1; then
     RSYNC=true
   fi
 
   # Build the extract CMD
   if [ "$RSYNC" == "false" ]; then
-    PULL_FILES="rm -f $FILE_DUMP && terminus backup:get $SITE.$FILES --element=files --to=$FILE_DUMP &&"
+    PULL_FILES="rm -f $FILE_DUMP && terminus backup:get $VERBOSITY $SITE.$FILES --element=files --to=$FILE_DUMP &&"
     PULL_FILES="$PULL_FILES pv $FILE_DUMP | tar xzf - -C $LANDO_WEBROOT/$FILEMOUNT --strip-components 1 &&"
   fi
 
--- scripts/push.sh
+++ scripts/push.sh
@@ -22,6 +22,7 @@ MESSAGE="My Awesome Lando-based commit"
 CODE=${TERMINUS_ENV:-dev}
 DATABASE=${TERMINUS_ENV:-dev}
 FILES=${TERMINUS_ENV:-dev}
+VERBOSITY=""
 
 # Set helpers
 FRAMEWORK=${FRAMEWORK:-drupal}
@@ -79,6 +80,14 @@ while (( "$#" )); do
         shift 2
       fi
       ;;
+    -vv|--verbose)
+        VERBOSITY="-vv"
+        shift
+      ;;
+    -vvv|--debug)
+        VERBOSITY="-vvv"  
+        shift
+      ;;
     --)
       shift
       break
@@ -113,23 +122,23 @@ fi
 if [ "$CODE" != "none" ]; then
   # Validate before we begin
   lando_pink "Validating you can push code to $CODE..."
-  terminus env:info $SITE.$CODE
+  terminus env:info $VERBOSITY $SITE.$CODE
   lando_green "Confirmed!"
 
   # Get connection mode
   lando_pink "Checking connection mode"
-  CONNECTION_MODE=$(terminus env:info $SITE.$CODE --field=connection_mode)
+  CONNECTION_MODE=$(terminus env:info $VERBOSITY $SITE.$CODE --field=connection_mode)
   # If we are not in git mode lets check for uncommited changes
   if [ "$CONNECTION_MODE" != "git" ]; then
     # Get the code diff
-    CODE_DIFF=$(terminus env:diffstat $SITE.$CODE --format=json)
+    CODE_DIFF=$(terminus env:diffstat $VERBOSITY $SITE.$CODE --format=json)
     if [ "$CODE_DIFF" != "[]" ]; then
       lando_yellow "Lando has detected you have uncommitted changes on Pantheon."
       lando_yellow "Please login to your Pantheon dashboard, commit those changes and then try lando push again."
       exit 5
     else
       lando_yellow "Changing connection mode to git for the pushy push"
-      terminus connection:set $SITE.$CODE git
+      terminus connection:set $VERBOSITY $SITE.$CODE git
     fi
   fi
   lando_green "Connected with git"
@@ -146,8 +155,8 @@ if [ "$CODE" != "none" ]; then
   fi
 
   # Set the git config if we need to
-  git config user.name "$(terminus auth:whoami --field='First Name') $(terminus auth:whoami --field='Last Name')"
-  git config user.email "$(terminus auth:whoami --field='Email')"
+  git config user.name "$(terminus auth:whoami $VERBOSITY --field='First Name') $(terminus auth:whoami $VERBOSITY --field='Last Name')"
+  git config user.email "$(terminus auth:whoami $VERBOSITY --field='Email')"
 
   # Commit the goods
   echo "Pushing code to $CODE as $(git config --local --get user.name) <$(git config --local --get user.email)> ..."
@@ -159,7 +168,7 @@ if [ "$CODE" != "none" ]; then
   # Set the mode back to what it was before because we are responsible denizens
   if [ "$CONNECTION_MODE" != "git" ]; then
     echo "Changing connection mode back to $CONNECTION_MODE"
-    terminus connection:set $SITE.$CODE $CONNECTION_MODE
+    terminus connection:set $VERBOSITY $SITE.$CODE $CONNECTION_MODE
   fi
 fi
 
@@ -167,17 +176,17 @@ fi
 if [ "$DATABASE" != "none" ]; then
   # Validate before we begin
   lando_pink "Validating you can push data to $DATABASE..."
-  terminus env:info $SITE.$DATABASE
+  terminus env:info $VERBOSITY $SITE.$DATABASE
   lando_green "Confirmed!"
 
   # Wake up the site so we can actually connect
   lando_pink "Making sure your database is awake!"
-  terminus env:wake $SITE.$DATABASE
+  terminus env:wake $VERBOSITY $SITE.$DATABASE
   lando_green "Awake!"
 
   # And push
   echo "Pushing your database... This miiiiight take a minute"
-  REMOTE_CONNECTION="$(terminus connection:info $SITE.$DATABASE --field=mysql_command)"
+  REMOTE_CONNECTION="$(terminus connection:info $VERBOSITY $SITE.$DATABASE --field=mysql_command)"
   PUSH_DB="mysqldump -u pantheon -ppantheon -h database --no-autocommit --single-transaction --opt -Q pantheon | $REMOTE_CONNECTION"
   eval "$PUSH_DB"
 fi
@@ -186,7 +195,7 @@ fi
 if [ "$FILES" != "none" ]; then
   # Validate before we begin
   lando_pink "Validating you can push files to $FILES..."
-  terminus env:info $SITE.$FILES
+  terminus env:info $VERBOSITY $SITE.$FILES
   lando_green "Confirmed!"
 
   # Build the rsync command
